157a4646fe900a0248856b198fb55f9e5bdf6cd0
Jonathan Leder <jonathan.roettger@web.de>
2022-11-04 16:25:03 +0100

    toggle answer button, toggle bookmark, create form

--- js/index.js
+++ js/index.js
@@ -4,11 +4,39 @@ const answerBox2 = document.querySelector('[data-js="box__answer2"]');
 const answerButton = document.querySelector('[data-js="show--answer1"]');
 const answerButton2 = document.querySelector('[data-js="show--answer2"]');
 const questionCard = document.querySelector('[data-js="single-question"]');
+const bookmarkClick = document.querySelector('[data-js="imgChange"]');
 
 answerButton.addEventListener("click", () => {
   answerBox.classList.toggle("box__answer1--hidden");
 });
 
+// answerButton.addEventListener("click", () => {
+//   const buttonText = "Show answer";
+
+//   if (answerBox.classList === "box__answer1--hidden") {
+//     answerButton.innerHTML = buttonText;
+//   } else {
+//     answerButton.innerHTML = "Hide Answer";
+//   }
+// });
+
+answerButton.addEventListener("click", (event) => {
+  event.target.textContent =
+    event.target.textContent == "Hide answer" ? "Show answer" : "Hide answer";
+});
+
 answerButton2.addEventListener("click", () => {
   answerBox2.classList.toggle("box__answer2--hidden");
+});
+
+function changeImage() {
+  if (bookmarkClick.src.match("/images/bookmark-default.png")) {
+    bookmarkClick.src = "/images/bookmark-gewaehlt.png";
+  } else {
+    bookmarkClick.src = "/images/bookmark-default.png";
+  }
+}
+
+bookmarkClick.addEventListener("click", () => {
+  changeImage();
 });
